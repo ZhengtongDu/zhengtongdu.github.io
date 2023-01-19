1b8858587292c139b9b34abdda804bd06dd575d1
zhengtongdu <zhengtongdu@zju.edu.cn>
2023-01-20 01:01:13 +0800

    fix gitignore

--- _site/FastPush.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -f ./_posts/*.html
-git add .
-git commit -m "$1"
-git push -u origin main
--- _site/OnceADay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -f ./_posts/*.html
-echo "have removed all the html file in the './_posts' directory"
-git add .
-git commit -m "The day is over."
-git push -u origin main
