184cfb230283f5fb88f1ef009b0a14fa71a6675c
zhengtongdu <zhengtongdu@zju.edu.cn>
2023-01-20 00:52:58 +0800

    The day is over.

--- _site/post_generate.sh
+++ _site/post_generate.sh
@@ -8,6 +8,6 @@ echo "---" >> $post
 echo "layout: post" >> $post 
 echo "title: $1" >> $post 
 echo "date: $tim +0800" >> $post 
-echo "tags: [learning, ]" >> $post 
+echo "tags: [\"Learning Notes\", ]" >> $post 
 echo "toc: true" >> $post 
 echo "---" >> $post 
